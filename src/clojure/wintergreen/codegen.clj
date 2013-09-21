c4f619dd3ac49b7caea18c68823b34b3fa63cf32
Brian Kauke <kaukeb@gmail.com>
2013-09-20 23:50:23 -0700

    Add docstrings to codegen namespace.

--- src/clojure/wintergreen/codegen.clj
+++ src/clojure/wintergreen/codegen.clj
@@ -1,77 +1,110 @@
 (ns wintergreen.codegen)
 
-(def indent-level (atom 0))
-(defn indent []  (swap! indent-level inc))
-(defn outdent [] (swap! indent-level #(max 0 (dec %))))
+(def indent-level "Current source-code indentation level." (atom 0))
 
-(defn print-indented-line [s]
+(defn indent "Increment current indentation level." []
+  (swap! indent-level inc))
+
+(defn outdent "Decrement current indentation level." []
+  (swap! indent-level #(max 0 (dec %))))
+
+(defn print-indented-line
+  "Print a string indented to the level defined in the var indent-level."
+  [s]
   (println (apply str (concat (repeat @indent-level "  ") s))))
 
-(defn print-statement [s]  
+(defn print-statement
+  "Print a string indented to current indent-level with trailing semicolon."
+  [s]
   (if (not (empty? s)) (print-indented-line (str s ";"))))
 
 (declare to-js)
 
-(defn program-to-js [program]
-  (doseq [statement program]
-    (let [expr (to-js statement)]
-      (if expr
-        (println (str expr ";"))))))
+(defn write-program-js
+  "Top-level driver to write a program tree as JavaScript source
+  code. Writes code to *out*."
+  [program]
+  (binding [indent-level (atom 0)]
+    (doseq [statement program]
+      (let [expr (to-js statement)]
+        (if expr
+          (println (str expr ";")))))))
 
-(defn program-to-js-string [program]
+(defn program-to-js
+  "Converts a program tree to JavaScript source code, returned as a string."
+  [program]
   (binding [*out* (new java.io.StringWriter)]
-    (program-to-js program)
+    (write-program-js program)
     (.toString *out*)))
 
-(defmulti to-js (fn [x] (if (list? x) (first x) 'scalar)))
+(defmulti write-js 
+  "Converts a program node to JavaScript source code. May return a
+  string or nil.
 
-(defmethod to-js 'proc [nodes]
+  In general, statement blocks are responsible for printing their code
+  to *out*, properly separating statements with semicolons and
+  newlines. Other tree nodes will return strings."
+  (fn [x] (if (list? x) (first x) 'scalar)))
+
+; Procedure.
+(defmethod write-js 'proc [nodes]
   (let [[_ name args & statements] nodes]
     (print-indented-line (format "var %s = function(%s) {"
                                  name
-                                 (apply str (interpose ", " (map to-js args)))))
+                                 (apply str (interpose ", " (map write-js args)))))
     (indent)
-    (dorun (map (comp print-statement to-js) statements))
+    (dorun (map (comp print-statement write-js) statements))
     (outdent)
     (print-indented-line "}")))
 
-(defmethod to-js 'arg [nodes] (str (nth nodes 2)))
+; Procedure argument.
+(defmethod write-js 'arg [nodes] (str (nth nodes 2)))
 
-(defmethod to-js 'return [nodes] (format "return %s" (to-js (second nodes))))
+; Return statement.
+(defmethod write-js 'return [nodes] (format "return %s" (write-js (second nodes))))
 
-(defmethod to-js 'call [nodes]
+; Procedure call.
+(defmethod write-js 'call [nodes]
   (let [[_ name & args] nodes]
-    (format "%s(%s)" name (apply str (interpose ", " (map to-js args))))))
+    (format "%s(%s)" name (apply str (interpose ", " (map write-js args))))))
 
-(defmethod to-js 'while [nodes]
+; While-loop.
+(defmethod write-js 'while [nodes]
   (let [[_ predicate & statements] nodes]
-    (print-indented-line (format "while (%s) {" (to-js predicate)))
+    (print-indented-line (format "while (%s) {" (write-js predicate)))
     (indent)
-    (dorun (map (comp print-statement to-js) statements))
+    (dorun (map (comp print-statement write-js) statements))
     (outdent)
     (print-indented-line "}")))
 
-(defmethod to-js 'decl [nodes]
+; Variable declaration.
+(defmethod write-js 'decl [nodes]
   (let [[_ type name & value] nodes]
     (if (empty? value)
       (format "var %s" name)
-      (format "var %s = %s" name (to-js (first value))))))
+      (format "var %s = %s" name (write-js (first value))))))
 
-(defmethod to-js 'assign [nodes]
+; Variable assignment.
+(defmethod write-js 'assign [nodes]
   (let [[_ varname value] nodes]
-    (format "%s = %s" varname (to-js value))))
+    (format "%s = %s" varname (write-js value))))
 
-(defmethod to-js 'binop [nodes]
+; Binary operation.
+(defmethod write-js 'binop [nodes]
   (let [[_ op a b] nodes]
-    (format "(%s %s %s)" (to-js a) op (to-js b))))
+    (format "(%s %s %s)" (write-js a) op (write-js b))))
 
-(defmethod to-js 'var [nodes] (str (second nodes)))
+; Variable.
+(defmethod write-js
+  'var [nodes] (str (second nodes)))
 
-(defmethod to-js 'scalar [s]
+; Single-value literal. May be a number or string.
+(defmethod write-js 'scalar [s]
   (if (string? s)
     (str "\"" s "\"") 
     (str s)))
 
-(defmethod to-js 'putTestValue [nodes]
+; Built-in function to provide a test result value to the testing framework.
+(defmethod write-js 'putTestValue [nodes]
   (let [[_ name value] nodes]
-    (format "testValues.put(%s, %s)" (to-js name) (to-js value))))
+    (format "testValues.put(%s, %s)" (write-js name) (write-js value))))
