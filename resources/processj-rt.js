300bdf141cfb139769bc32942b2e5d5f94c8c1ca
Brian Kauke <kaukeb@gmail.com>
2013-11-14 17:26:21 -0800

    Update runtime sketch

--- resources/processj-rt.js
+++ resources/processj-rt.js
@@ -3,6 +3,8 @@ __RT = function() {
     // Scheduler logic
     //////
     
+    var processes = {};
+    
     var runQueue = [];
     
     var schedule = function(p) {
@@ -25,13 +27,21 @@ __RT = function() {
     // Process
     //////
     
-    function Process() {
+    function Process(name) {
+	this.name = name;
 	this.commData = null;
+	processes[name] = this;
     }
-
+    
     Process.prototype.start = function() {
 	schedule(this);
     }
+
+    Process.prototype.toString = function() {
+	return this.name;
+    }
+
+    Process.prototype.instanceCounter = 0;
     
     //////
     // Channel
@@ -74,6 +84,57 @@ __RT = function() {
     }
     
     //////
+    // Barrier
+    /////
+
+    function Barrier() {
+	this.enrolledProcs = {};
+	this.nEnrolled = 0;
+	this.waitingProcs = {};
+	this.nWaiting = 0;
+    }
+    
+    Barrier.prototype.enroll = function(p){
+	this.enrolledProcs[p] = true;
+	this.nEnrolled++;
+    }
+    
+    Barrier.prototype.resign = function(p) {
+	if (p in this.enrolledProcs) {
+	    delete this.enrolledProcs[p];
+	    this.nEnrolled--;
+	}
+    }
+
+    Barrier.prototype.sync = function(p) {
+	// If process is not enolled, this is an error.
+	//console.log(this.enrolledProcs);
+	//console.log(p.toString() in Object.keys(this.enrolledProcs));
+	if (!(p in this.enrolledProcs)) {
+	    return;
+	}
+
+	// Add process to wait list
+	if (!(p in this.waitingProcs)) {
+	    this.waitingProcs[p] = true;
+	    this.nWaiting++;
+	}
+	
+	// Check if barrier is completed
+	if (this.nEnrolled === this.nWaiting) {
+	    console.log("barrier completed");
+	    // Allow all processes to continue
+	    for (var pname in this.enrolledProcs) {
+		//console.log(pname);
+		schedule(processes[pname]);
+	    }
+	    // Clear wait list
+	    this.waitingProcs = {};
+	    this.nWaiting = 0;
+	}
+    }
+    
+    //////
     // Public interface
     //////
     
@@ -81,23 +142,28 @@ __RT = function() {
 	helloWorld : function() { console.log("Hello, world."); },
 	Process : Process,
 	Channel : Channel,
+	Barrier : Barrier,
 	main : main
     };
 }();
 
 var c1 = new __RT.Channel();
+var b1 = new __RT.Barrier();
 
 ////// Test process Recv, receives a value on c1
 var Recv = function() {
-    __RT.Process.call(this);
+    __RT.Process.call(this, "Recv$" + (Recv.prototype.instanceCounter++));
     this.a = 0;
     this.b = 0;
+    this.c = 0;
     this.next = Recv.prototype.slice0;
 }
 Recv.prototype = new __RT.Process();
 Recv.prototype.constructor = Recv;
+Recv.prototype.instanceCounter = 0;
 
 Recv.prototype.slice0 = function() {
+    b1.enroll(this);
     this.a += 1;
     this.next = Recv.prototype.slice1;
     c1.read(this);
@@ -106,23 +172,38 @@ Recv.prototype.slice0 = function() {
 Recv.prototype.slice1 = function() {
     this.a = this.commData;
     this.b = this.a * 2;
+    this.next = Recv.prototype.slice2;
+    b1.sync(this);
+}
+
+Recv.prototype.slice2 = function() {
+    this.c = 555;
     this.next = null;
 }
+
 //////
 
 ////// Test process Send, sends a value on c1
 var Send = function() {
-    __RT.Process.call(this);
+    __RT.Process.call(this, "Send$" + (Send.prototype.instanceCounter++));
     this.next = Send.prototype.slice0;
 }
 Send.prototype = new __RT.Process();
 Send.prototype.constructor = Send;
+Send.prototype.instanceCounter = 0;
 
 Send.prototype.slice0 = function() {
+    b1.enroll(this);
     this.commData = 999;
-    this.next = null;
+    this.next = Send.prototype.slice1;
     c1.write(this);
 }
+
+Send.prototype.slice1 = function() {
+    this.next = null;
+    b1.sync(this);
+}
+
 //////
 
 s = new Send();
@@ -137,3 +218,4 @@ __RT.main();
 console.log(r.a);
 console.log(r.b);
 
+console.log(r.c);
